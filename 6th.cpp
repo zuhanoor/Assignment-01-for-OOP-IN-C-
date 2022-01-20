d55a00ed05a9c1e96c026d9333c79b897353a4fd
zuhanoor <95204939+zuhanoor@users.noreply.github.com>
2022-01-20 20:32:31 +0530

    Add files via upload

---
+++ 6th.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int guests, chairs;
+    long arrangements = 1;
+
+    cout << "Number of guests: ";
+    cin >> guests;
+
+    cout << "Number of chairs: ";
+    cin >> chairs;
+
+    for (int i = 1; i <= chairs; i++)
+        arrangements *= guests--;
+
+    cout << "------------------" << endl;    
+    cout << "Number of arrangements possible: " << arrangements;
+
+    return 0;
+}
