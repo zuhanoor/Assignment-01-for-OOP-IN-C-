37261a0722da775b514e333f401daec8151fd537
zuhanoor <95204939+zuhanoor@users.noreply.github.com>
2022-05-21 21:18:25 +0530

    Create program 9.cpp

---
+++ program 9.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number,smallest, largest;
+    double sum=0, average;
+    int arr[number];
+    cout << "Number of values: ";
+    cin >> number;
+
+  
+    cout << "Enter the numbers:";arr;
+    for (int i = 0; i < number; i++)
+        cin >> arr[i];
+    smallest = largest = arr[0];
+
+    for (int i = 0; i < number; i++)
+    {
+        sum += arr[i];
+
+        if (arr[i] < smallest)
+            smallest = arr[i];
+        
+        if (arr[i] > largest)
+            largest = arr[i];
+    }
+
+    average = sum / number;
+
+   
+    cout << "Smallest Number is: " << smallest << endl;
+    cout << "Largest Number is: " << largest << endl;
+    cout << "Average Number is: " << average << endl;
+
+    return 0;
+}
